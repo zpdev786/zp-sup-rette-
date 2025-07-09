3396fe45568625bc77ad8ea8b0ba29881b72fc9a
ZP Développement <102963115+zpdev786@users.noreply.github.com>
2025-07-09 13:37:39 +0200

    Add files via upload

---
+++ client/shop.lua
@@ -0,0 +1,126 @@
+local ESX = exports['es_extended']:getSharedObject()
+
+-- Spawn des peds + ox_target + blip
+CreateThread(function()
+    for i, coords in ipairs(Config.Pos) do
+        local model = joaat(Config.PedModel)
+        RequestModel(model)
+        while not HasModelLoaded(model) do Wait(10) end
+
+        local ped = CreatePed(0, model, coords.xyz, coords.w, false, true)
+        SetEntityInvincible(ped, Config.PedInvincible)
+        FreezeEntityPosition(ped, Config.PedFrozen)
+        SetBlockingOfNonTemporaryEvents(ped, true)
+
+        if Config.UseOxTarget then
+            exports.ox_target:addLocalEntity(ped, {
+                {
+                    name = 'shop_' .. i,
+                    icon = 'fa-solid fa-shop',
+                    label = Config.all.titletarget,
+                    onSelect = function()
+                        OpenShopMenu()
+                    end
+                }
+            })
+        else
+            -- Notification "Appuyez sur E"
+    local inZone = false
+
+    lib.zones.sphere({
+        coords = coords.xyz,
+        radius = 2.5,
+        debug = false,
+        onEnter = function()
+            inZone = true
+            lib.showTextUI('[E] - ' .. Config.all.title, {
+                position = Config.all.placenotif,
+                icon = 'fa-solid fa-hand-point-right',
+                style = {
+                    borderRadius = 1,
+                    backgroundColor = Config.all.colornotif,
+                    color = 'white'
+                }
+            })
+        end,
+        onExit = function()
+            inZone = false
+            lib.hideTextUI()
+        end
+    })
+
+    CreateThread(function()
+        while true do
+            Wait(0)
+            if inZone and IsControlJustPressed(0, 38) then -- E
+                OpenShopMenu()
+            end
+        end
+    end)
+end
+
+
+            if Config.ShowBlips then
+                local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
+                SetBlipSprite(blip, Config.Blip.shop) -- Magasin
+                SetBlipDisplay(blip, Config.Blip.affichage)
+                SetBlipScale(blip, Config.Blip.tailleshop)
+                SetBlipColour(blip, Config.Blip.couleurshop)
+                SetBlipAsShortRange(blip, true)
+                BeginTextCommandSetBlipName("STRING")
+                AddTextComponentString(Config.all.title)
+                EndTextCommandSetBlipName(blip)
+            end
+        end
+end)
+
+-- Ouvre le menu de sélection d'items
+function OpenShopMenu()
+    local options = {}
+
+    for _, item in pairs(Config.Items) do
+        table.insert(options, {
+            title = item.label,
+            description = string.format("%s\nPrix unitaire : %s$", item.description, item.price ),
+            icon = 'box',
+            onSelect = function()
+                OpenQuantityMenu(item)
+            end
+        })
+    end
+
+    lib.registerContext({
+        id = 'zejay_shop',
+        title = Config.all.title,
+        options = options
+    })
+
+    lib.showContext('zejay_shop')
+end
+
+-- Ouvre le menu pour choisir la quantité
+function OpenQuantityMenu(item)
+    local input = lib.inputDialog(('Acheter : %s'):format(item.label), {
+        {
+            type = 'number',
+            label = 'Quantité',
+            description = 'Combien voulez-vous en acheter ?',
+            required = true,
+            icon = 'hashtag',
+            min = 1
+        }
+    })
+
+    if input and input[1] then
+        local quantity = tonumber(input[1])
+        if quantity and quantity > 0 then
+            TriggerServerEvent('zejay:buyItem', item.name, quantity)
+        else
+            lib.notify({
+                title = 'Erreur',
+                description = 'Quantité invalide.',
+                type = 'error'
+            })
+        end
+    end
+end
